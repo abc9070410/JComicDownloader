ed2b77ea92970f60a1a802314152b9f100aaf14c
Kenny <me@kennyforge.com>
2015-07-30 12:09:48 +0800

    improve test result message

--- src/test/java/jcomicdownloader/TestURL.java
+++ src/test/java/jcomicdownloader/TestURL.java
@@ -8,7 +8,6 @@ package jcomicdownloader;
 import static org.junit.Assert.*;
 import java.util.*;
 import jcomicdownloader.module.*;
-import org.junit.*;
 import org.junit.runner.*;
 import org.junit.runners.*;
 
@@ -17,37 +16,37 @@ import org.junit.runners.*;
  * @author apple
  */
 @RunWith(value=Parameterized.class)
-
 public class TestURL {
    
     @Parameterized.Parameters
     public static Collection<String[]> getParameters() {
         return Arrays.asList(
             new String[][] {
-                {"http://www.comicvip.com/html/9718.html","EC","jcomicdownloader.module.ParseEC"},
-                {"http://manhua.dmzj.com/ganwumeixiaomai/","178","jcomicdownloader.module.Parse178"},
-                {"http://comic.sfacg.com/HTML/GWMXM/","SF","jcomicdownloader.module.ParseSF"},
-                {"http://comic.kukudm.com/comiclist/1842/index.htm","KUKU","jcomicdownloader.module.ParseKUKU"},
-                {"http://comic.ck101.com/comic/18181","CK","jcomicdownloader.module.ParseCK"}
+                {"http://www.comicvip.com/html/9718.html","jcomicdownloader.module.ParseEC"},
+                {"http://manhua.dmzj.com/ganwumeixiaomai/","jcomicdownloader.module.Parse178"},
+                {"http://comic.sfacg.com/HTML/GWMXM/","jcomicdownloader.module.ParseSF"},
+                {"http://comic.kukudm.com/comiclist/1842/index.htm","jcomicdownloader.module.ParseKUKU"},
+                {"http://comic.ck101.com/comic/18181","jcomicdownloader.module.ParseCK"}
             }
         );
     }
-    private ParseOnlineComicSite s;
-    private String url,name,mod;
+    private final String url,mod;
         
-    public TestURL(String url, String name, String mod) {
+    public TestURL(String url, String mod) {
         this.url = url;
-        this.name = name;
         this.mod = mod;
     }
         
     @org.junit.Test
     public void testUrl() {
+        ParseOnlineComicSite s;
         boolean b =false;
         try{
             s=(ParseOnlineComicSite)Class.forName(mod).newInstance(); 
             b=s.canParserHandle(url);
-        }catch(Exception x){}
-        assertEquals(name, true,b); 
+            assertTrue(mod,b); 
+        }catch(Exception x){
+            fail("Module "+mod+" has exception "+x.getMessage());           
+        }
     }
 }
