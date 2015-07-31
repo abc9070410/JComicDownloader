1edfe197313313c7ae112f82d9c51fe022831e61
Kenny <me@kennyforge.com>
2015-07-31 23:10:32 +0800

    Only focus test ParseTUKU

--- src/test/java/jcomicdownloader/TestURL.java
+++ src/test/java/jcomicdownloader/TestURL.java
@@ -9,10 +9,10 @@ import static org.junit.Assert.*;
 import java.util.*;
 import jcomicdownloader.module.*;
 import jcomicdownloader.tools.Common;
-import org.junit.Assert;
 import org.junit.runner.*;
 import org.junit.runners.*;
 import org.junit.*;
+import org.junit.rules.*;
 
 /**
  *
@@ -21,20 +21,37 @@ import org.junit.*;
 @RunWith(value=Parameterized.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestURL {
-   
+    @Rule 
+    public TestRule watcher = new TestWatcher() {
+        protected void starting(Description description) {
+          System.out.println("Starting test: " + description.getMethodName());
+        }
+        @Override
+        protected void failed(Throwable e, Description description) {
+           System.out.println("Test fail: "  + mod +"\t"+ description.getMethodName() + "\n");
+        }
+
+        @Override
+        protected void succeeded(Description description) {
+            System.out.println("Test success: " +  mod +"\t"+ description.getMethodName() + "\n");
+           }
+       };
+       
     @Parameterized.Parameters
     public static Collection<Object[]> getParameters() {
         return Arrays.asList(
             new Object[][] {               
-                 {0,"http://www.comicvip.com/html/9718.html","jcomicdownloader.module.ParseEC"}
-                ,{1,"http://manhua.dmzj.com/ganwumeixiaomai/","jcomicdownloader.module.Parse178"}
-                ,{2,"http://comic.sfacg.com/HTML/GWMXM/","jcomicdownloader.module.ParseSF"}
-                ,{3,"http://comic.kukudm.com/comiclist/1842/index.htm","jcomicdownloader.module.ParseKUKU"}
-                ,{4,"http://comic.ck101.com/comic/18181","jcomicdownloader.module.ParseCK"}
-                ,{5,"http://www.tuku.cc/comic/11977/","jcomicdownloader.module.ParseTUKU"}               
+//                 {0,"http://www.comicvip.com/html/9718.html","jcomicdownloader.module.ParseEC"}
+//                ,{1,"http://manhua.dmzj.com/ganwumeixiaomai/","jcomicdownloader.module.Parse178"}
+//                ,{2,"http://comic.sfacg.com/HTML/GWMXM/","jcomicdownloader.module.ParseSF"}
+//                ,{3,"http://comic.kukudm.com/comiclist/1842/index.htm","jcomicdownloader.module.ParseKUKU"}
+//                ,{4,"http://comic.ck101.com/comic/18181","jcomicdownloader.module.ParseCK"}
+//                ,
+                {5,"http://www.tuku.cc/comic/11977/","jcomicdownloader.module.ParseTUKU"}               
             }
         );
     }
+    
     private final String url,mod;
     private ParseOnlineComicSite s;
 
@@ -42,16 +59,17 @@ public class TestURL {
         this.url = url;
         this.mod = mod;;        
         Run.isAlive=true;// tweak for no download
-        Common.debugPrintln("-----------------"+i+"--------------------");
         Common.consoleThreadName= Thread.currentThread().getName(); // tweak for no GUI
+        Common.debugPrintln("-----------------"+i+"--------------------");
         try{
             this.s=(ParseOnlineComicSite)Class.forName(mod).newInstance(); 
-            this.s.setURL(url);// will fail if not set in some case            
+            this.s.setURL(url);// will fail if not set in some case , i think it is a bug       
         }catch(Exception x){
             this.s=null;
-            fail("Module "+mod+" has exception "+x.getMessage());           
+            fail("Module "+mod+" has exception : "+x.getMessage());           
         }
     }
+
         
     @org.junit.Test
     public void test001Url() {
@@ -79,6 +97,6 @@ public class TestURL {
         String allPageString = s.getAllPageString(url); 
         String title=s.getTitleOnMainPage(url, allPageString);
         System.out.println("test004getTitleOnMainPage : "+title);
-        assertNotNull(title );
+        assertNotNull(title);
     }
 }
