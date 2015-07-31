acc7d8b5b9e44bf1fb79aa8dd3250e400d7c2a17
Kenny <me@kennyforge.com>
2015-07-31 19:24:47 +0800

    improve JUnit test case

--- src/test/java/jcomicdownloader/TestURL.java
+++ src/test/java/jcomicdownloader/TestURL.java
@@ -12,24 +12,26 @@ import jcomicdownloader.tools.Common;
 import org.junit.Assert;
 import org.junit.runner.*;
 import org.junit.runners.*;
+import org.junit.*;
 
 /**
  *
  * @author apple
  */
 @RunWith(value=Parameterized.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestURL {
    
     @Parameterized.Parameters
     public static Collection<Object[]> getParameters() {
         return Arrays.asList(
-            new Object[][] {
-                {1,"http://www.comicvip.com/html/9718.html","jcomicdownloader.module.ParseEC"},
-                {2,"http://manhua.dmzj.com/ganwumeixiaomai/","jcomicdownloader.module.Parse178"},
-                {3,"http://comic.sfacg.com/HTML/GWMXM/","jcomicdownloader.module.ParseSF"},
-                {4,"http://comic.kukudm.com/comiclist/1842/index.htm","jcomicdownloader.module.ParseKUKU"},
-                {5,"http://comic.ck101.com/comic/18181","jcomicdownloader.module.ParseCK"},
-                {6,"http://www.tuku.cc/comic/11977/","jcomicdownloader.module.ParseTUKU"}
+            new Object[][] {               
+                 {0,"http://www.comicvip.com/html/9718.html","jcomicdownloader.module.ParseEC"}
+                ,{1,"http://manhua.dmzj.com/ganwumeixiaomai/","jcomicdownloader.module.Parse178"}
+                ,{2,"http://comic.sfacg.com/HTML/GWMXM/","jcomicdownloader.module.ParseSF"}
+                ,{3,"http://comic.kukudm.com/comiclist/1842/index.htm","jcomicdownloader.module.ParseKUKU"}
+                ,{4,"http://comic.ck101.com/comic/18181","jcomicdownloader.module.ParseCK"}
+                ,{5,"http://www.tuku.cc/comic/11977/","jcomicdownloader.module.ParseTUKU"}               
             }
         );
     }
@@ -39,8 +41,12 @@ public class TestURL {
     public TestURL(Integer i,String url, String mod) {
         this.url = url;
         this.mod = mod;;        
+        Run.isAlive=true;// tweak for no download
+        Common.debugPrintln("-----------------"+i+"--------------------");
+        Common.consoleThreadName= Thread.currentThread().getName(); // tweak for no GUI
         try{
             this.s=(ParseOnlineComicSite)Class.forName(mod).newInstance(); 
+            this.s.setURL(url);// will fail if not set in some case            
         }catch(Exception x){
             this.s=null;
             fail("Module "+mod+" has exception "+x.getMessage());           
@@ -48,33 +54,31 @@ public class TestURL {
     }
         
     @org.junit.Test
-    public void testUrl() {
-        assertTrue(mod,s.canParserHandle(url)); 
+    public void test001Url() {
+        assertTrue(mod,s.canParserHandle(url));
     }
     
     @org.junit.Test
-      public void testAllPageString(){
-          String str ;
-                      Common.debugPrintln("Test URL : "+url);
-
-          str= s.getAllPageString(url) ;
-          Assert.assertNotNull(mod, str);
-    }
-      
-    @org.junit.Test
-    public void testGetTitle(){
-        Assert.assertNotNull(mod, s.getTitleOnMainPage(url,s.getAllPageString(url)));        
-    }
-    
-    @org.junit.Test
-    public void testSetParamters() {
-        boolean b = true;
+    public void test002setParameters() {
         try{
             s.setParameters();
-            assertTrue(b);
-        }catch(Exception x){
-            fail("Module "+mod+" has exception "+x.getMessage());           
+        }catch(Exception e){
+            e.printStackTrace();
+            fail(e.toString()+ " : "+e.getMessage());
         }
     }
-
+    
+    @org.junit.Test
+    public void test003getAllPageString() {    
+        String allPageString = s.getAllPageString(url);
+        assertNotNull(allPageString);
+    }
+    
+    @org.junit.Test
+    public void test004getTitleOnMainPage() {
+        String allPageString = s.getAllPageString(url); 
+        String title=s.getTitleOnMainPage(url, allPageString);
+        System.out.println("test004getTitleOnMainPage : "+title);
+        assertNotNull(title );
+    }
 }
